a2fcf1aff5cc5a29ec8bb13022da7b262b01db74
zeeshan363 <zsarfraz363@gmail.com>
2026-02-03 14:09:36 +0500

    feat: added all modules required for project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
+import { BalancesModule } from './balances/balances.module';
+import { ExpensesModule } from './expenses/expenses.module';
+import { GroupsModule } from './groups/groups.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, GroupsModule, ExpensesModule, BalancesModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
